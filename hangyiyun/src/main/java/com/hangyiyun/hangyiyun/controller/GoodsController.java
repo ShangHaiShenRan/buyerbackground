7beb173f9f8d05b276b8fdd72b422fc7f7c97b24
wangchenchen123 <2504649360@qq.com>
2020-04-13 10:53:21 +0800

    测试git提交问题-第二次提交:  1.单纯提交代码;

--- hangyiyun/src/main/java/com/hangyiyun/hangyiyun/controller/GoodsController.java
+++ hangyiyun/src/main/java/com/hangyiyun/hangyiyun/controller/GoodsController.java
@@ -28,6 +28,9 @@ import java.util.Map;
 @RequestMapping("/Goods")
 public class GoodsController {
 
+
+    int test = 0;
+
     final String HOST="http://xyyapi.michain.tech";
 
     private static final Logger logger = LoggerFactory.getLogger(MallController.class);
